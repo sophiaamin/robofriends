6bda594f901918e4621c10076e5bed91755ba70a
Sophia Amin <samin@shipt.com>
2020-12-29 21:45:09 -0800

    use fetch

--- src/App.js
+++ src/App.js
@@ -14,7 +14,9 @@ class App extends Component {
   }
 
   componentDidMount() {
-    this.setState({ robots: robots })
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ robots: users }))
   }
 
   onSearchChange = (event) => {
@@ -27,14 +29,17 @@ class App extends Component {
         .toLowerCase()
         .includes(this.state.searchfield.toLowerCase())
     })
-
-    return (
-      <div className="tc">
-        <h1 className="f2">RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={filterRobots} />
-      </div>
-    )
+    if (this.state.robots.length === 0) {
+      return <h1>Loading</h1>
+    } else {
+      return (
+        <div className="tc">
+          <h1 className="f2">RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <CardList robots={filterRobots} />
+        </div>
+      )
+    }
   }
 }
 
